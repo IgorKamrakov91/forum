33b0da1289b4a6d235d88a3e69495e4d68cbaeb9
IgorKamrakov <i.kamrakov@yandex.ru>
2016-12-15 20:20:43 +0300

    Add safe delete for ForumThread with paranoia gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209130500) do
+ActiveRecord::Schema.define(version: 20161215171149) do
 
   create_table "forum_posts", force: :cascade do |t|
     t.integer  "forum_thread_id"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20161209130500) do
     t.string   "subject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_forum_threads_on_deleted_at"
   end
 
   create_table "users", force: :cascade do |t|
